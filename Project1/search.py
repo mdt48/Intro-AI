43256fe6bf9c0b6d8ff81721820fb1170915a81a
Marshall Thompson <mdt_@gwu.edu>
2021-02-08 17:10:13 -0500

    uninformed search done

--- Project1/search.py
+++ Project1/search.py
@@ -1,8 +1,60 @@
-file = "p1_graph.txt"
+import operator
+def dijkstra(adj_list, S, E, node_to_block):
+    # all nodes unvisited
+    Q = {i for i in range(0, 100)}
+
+    #visited list
+    sp = []
+
+    # distance and prev dicts
+    dist = {}
+    prev = {}
+
+    u = None
+    for vertex in adj_list.keys():
+        dist[vertex] = float('inf')
+        prev[vertex] = None
+
+    dist[S] = 0
+
+    while Q:
+        u = max(dist, key=dist.get); m = dist[u]
+        for k in dist.keys():
+            if dist[k] < m and k not in sp:
+                m = dist[k]
+                u = k
+
+        print(sp)
+        sp.append(u)
+        Q.remove(u)
+
+        if u == E:
+            break
+
+        for edge in adj_list[u]:
+            alt = dist[u] + edge[1]
+            if alt < dist[edge[0]]:
+                dist[edge[0]] = alt
+                prev[edge[0]] = u
+    
+    # print the shortest path
+    seq = []
+    if prev[u] != None or u == S:
+        while u != None:
+            seq.insert(0, u)
+            u = prev[u]
+    print("Nodes Visited: ", sp)
+    print("Path:", seq)
+    print("Length: {}".format(dist[E]))
+            
+file = "./p1_graph.txt"
 section = 0 # 0: nodes, 1: edges, 2: SE
 adj_list = {}
+node_to_block = {}
 S, E = None, None 
 
+
+# Read in data
 with open(file, "r") as graph:
     for f in graph:
         if f.startswith("#"): continue 
@@ -12,7 +64,9 @@ with open(file, "r") as graph:
 
         data = f.split(",")
 
-        if section == 0: adj_list[int(data[0])] = []
+        if section == 0: 
+            node_to_block[int(data[0])] = int(data[1])
+            adj_list[int(data[0])] = []
 
         if section == 1: 
             # undirected, add both ways
@@ -26,5 +80,4 @@ with open(file, "r") as graph:
                 E = int(data[1])
 
 
-print(adj_list)
-print(S, E)
+dijkstra(adj_list, S, E, node_to_block)
